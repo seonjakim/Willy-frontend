bb03ce5dbdd7669e6e0796b2bea89a3f0748042f
SJ <seonja.kim0@gmail.com>
2020-05-09 18:34:40 +0900

    the initial setting for 2nd projects

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = () => {
+    return <MainWrap />;
+};
+
+const MainWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default Main;
---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { GlobalStyle } from "./Config";
+import Main from "./Pages/Main/Main";
+
+class Routes extends React.Component {
+    render() {
+        return (
+            <Router>
+                <GlobalStyle />
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default Routes;
