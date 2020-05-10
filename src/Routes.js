601a8efbf9cfb8b01099d558a00a5b1bcfacbef6
SJ <seonja.kim0@gmail.com>
2020-05-10 14:20:47 +0900

    update reset.scss in Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalStyle } from "./Config";
+
 import Main from "./Pages/Main/Main";
+import SignIn from "./Pages/SignIn/SignIn"
 
 class Routes extends React.Component {
     render() {
@@ -10,6 +12,7 @@ class Routes extends React.Component {
                 <GlobalStyle />
                 <Switch>
                     <Route exact path="/" component={Main} />
+                    <Route exact path="/signin" component={SignIn} />
                 </Switch>
             </Router>
         );
