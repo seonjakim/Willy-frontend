0aa0e2f7eb8bb6fb2dc119433e57e449ff2d8ce4
SJ <seonja.kim0@gmail.com>
2020-05-10 14:39:20 +0900

    resolve error in color.js file

--- src/Styles/color.js
+++ src/Styles/color.js
@@ -3,11 +3,4 @@ const color = {
     borderColor: "#707070",
 }
 
-const loginColor = {
-    lightFont: "rgba(51,51,51,0.6)", //회원가입,
-    inputColor: "#d7d7d7", //회원가입
-    borderBottom: "rgba(1,19,108,0.04)", //회원가입 보더 바텀
-}
-
-export default loginColor;
 export default color;
